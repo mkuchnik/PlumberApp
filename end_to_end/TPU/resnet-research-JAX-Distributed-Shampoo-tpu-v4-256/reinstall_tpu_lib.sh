139f2f5b67c086b9c77fb14df9b2df15e17854be
Michael Kuchnik <michaelkuchnik@gmail.com>
2022-03-05 19:26:20 -0500

    Remove temporary file if it already exists

--- end_to_end/TPU/resnet-research-JAX-Distributed-Shampoo-tpu-v4-256/reinstall_tpu_lib.sh
+++ end_to_end/TPU/resnet-research-JAX-Distributed-Shampoo-tpu-v4-256/reinstall_tpu_lib.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# Remove the file, if it exists
+sudo rm /tmp/libtpu.so
+# Copy the libtpu file to /tmp
 cp $HOME/miniconda3/envs/py37/lib/python3.7/site-packages/libtpu/libtpu.so /tmp
+# Copy the libtpu file to system path
 sudo cp /tmp/libtpu.so /usr/lib/
